3789948f55a52e0ea490c69b69fa8a1464440cda
Jovan Petrov <jpetrov@gmail.com>
2019-11-25 00:21:37 +0100

    Update syntax in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,30 +1,36 @@
 # Python calculator v.1.0
 import math
 
+
 # Calculator functions
 
 # Addition
 def calc_add(x, y):
     return x + y
 
+
 # Subtraction
-def calc_subtract (x, y):
+def calc_subtract(x, y):
     return x - y
 
-# Muultiplication
-def calc_multiply (x, y):
+
+# Multiplication
+def calc_multiply(x, y):
     return x * y
 
+
 # Division
-def calc_divide (x, y):
+def calc_divide(x, y):
     return x / y
 
+
 # Exponentiation
-def calc_exponent (x, y):
+def calc_exponent(x, y):
     return x ** y
 
+
 # Square root
-def calc_sqrt (x):
+def calc_sqrt(x):
     return math.sqrt(x)
 
 
@@ -32,25 +38,25 @@ print('Welcome to the calculator v.1.0\n')
 print('Operators\n')
 print('+ = add, - = subtract, * = multiply, / = divide, ^ raise to power, s = square root\n')
 choice = 'y'
-while(choice == 'y' or choice =='Y'):
+while choice == 'y' or choice == 'Y':
     number1 = float(input("x = "))
     number2 = float(input("y = "))
     operator = str(input("Operator:"))
-    if(operator=='+'):
+
+    if operator == '+':
         result = calc_add(number1, number2)
-    elif(operator=='-'):
+    elif operator == '-':
         result = calc_subtract(number1, number2)
-    elif(operator=='/'):
+    elif operator == '/':
         result = calc_divide(number1, number2)
-    elif(operator=='*'):
+    elif operator == '*':
         result = calc_multiply(number1, number2)
-    elif(operator=='^'):
+    elif operator == '^':
         result = calc_exponent(number1, number2)
-    elif(operator=="s"):
+    elif operator == "s":
         result = calc_sqrt(number1)
     else:
-        print('Uknown operator')
+        print('Unknown operator')
         result = 0
-    print('The result is: '+str(result))
+    print('The result is: ' + str(result))
     choice = str(input('Would you like to make another calculation? For yes enter y :'))
-
